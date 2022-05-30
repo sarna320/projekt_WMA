26ce2a8d3b471319c99b47843ca78bfe7df08b3f
sarna320 <sarnacki99@wp.pl>
2022-05-30 15:02:06 +0200

    chyba ostateczna wersja

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+"---------------------------------------------------------------------------------------------------------------------"
 import cv2
 import numpy as np
 import urllib.request
-
+"---------------------------------------------------------------------------------------------------------------------"
 #funckja zwraca obraz mozliwy do przetwarzania przez opencv, ktory zostal pobrany z githuba
 #argumentem wejsciowym jest numer linku
 def zwrocObrazGithub(i):
@@ -22,10 +23,10 @@ def zwrocObrazGithub(i):
     return obraz
 
 #funckja do drukowania obrazu
-#argumentem wejsciowym jest obraz do pokazania
-def drukujObraz(obraz):
+#argumentem wejsciowym jest nazwa okna i obraz do pokazania
+def drukujObraz(nazwa,obraz):
     # pokazanie obrazu
-    cv2.imshow("obraz", obraz)
+    cv2.imshow(nazwa, obraz)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
@@ -88,18 +89,121 @@ def rodziel(obraz):
 
     return obraz_zloty, obraz
 
+#funkcja zwraca obraz z wykrytymi nominalami i sume nominalow wszystkich monet
+#argumentem wejsciowym jest obraz z oryginalny, obraz z zlotymi monetami i obraz z srebnymi monetami na czarnym tle
+def licz(obraz,obraz_zloty,obraz_srebny):
+    #zliczona wartosc monet na zdjeciu
+    suma=0
+
+    # konwersja na skale szarosci
+    gray_srebny = cv2.cvtColor(obraz_srebny, cv2.COLOR_BGR2GRAY)
+    thresh_srebny= cv2.adaptiveThreshold(gray_srebny, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101,-10)
+    gray_zloty = cv2.cvtColor(obraz_zloty, cv2.COLOR_BGR2GRAY)
+
+    #znalezienie zlotych monet
+    circles = cv2.HoughCircles(gray_zloty, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=20, maxRadius=45)
+
+    #tutaj rowniez wystupuje ten sam problem opisany juz wczesniej
+    #zliczanie sumy zlotych monet i detekcja na obrazie
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            #rysowanie kolek dla wszystkich
+            cv2.circle(obraz, (i[0], i[1]), i[2], (0, 255, 0), 2)
+
+            #dla 1 gr
+            if i[2]==27:
+                cv2.putText(img=obraz, text="1 gr", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                            fontScale=0.5,
+                            color=(0, 255, 0), thickness=1)
+                suma=suma+0.01
+
+            #dla 2 gr
+            if i[2]==30:
+                cv2.putText(img=obraz, text="2 gr", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                            fontScale=0.5,
+                            color=(0, 255, 0), thickness=1)
+                suma=suma+0.02
+
+            #dla 5 gr
+            if i[2]==34:
+                cv2.putText(img=obraz, text="5 gr", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                            fontScale=0.5,
+                            color=(0, 255, 0), thickness=1)
+                suma=suma+0.05
+
+            #dla 2 zl
+            if i[2]==37:
+                cv2.putText(img=obraz, text="2 zl", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                            fontScale=0.5,
+                            color=(0, 255, 0), thickness=1)
+                suma=suma+2.0
+
+            #dla 5 zl
+            if i[2]==28:
+                cv2.putText(img=obraz, text="5 zl", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                            fontScale=0.5,
+                            color=(0, 255, 0), thickness=1)
+                suma=suma+5.0
+
+
+    #znalezienie srebnych monet
+    circles1 = cv2.HoughCircles(thresh_srebny, cv2.HOUGH_GRADIENT, 1, 100, param1=300, param2=10, minRadius=25, maxRadius=45)
+
+    #tutaj rowniez wystupuje ten sam problem opisany juz wczesniej
+    #zliczanie sumy srebnych monet i detekcja na obrazie
+    if circles1 is not None:
+        circles1 = np.uint16(np.around(circles1))
+        for i in circles1[0, :]:
+            cv2.circle(obraz, (i[0], i[1]), i[2], (0, 255, 0), 2)
+
+            #dla 10 gr
+            if i[2]==31 or i[2]==30:
+                cv2.putText(img=obraz, text="10 gr", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                        fontScale=0.5,
+                        color=(0, 255, 0), thickness=1)
+                suma=suma+0.10
+
+            #dla 20 gr
+            if i[2]==35 or i[2]==34 or i[2]==33 or i[2]==32:
+                cv2.putText(img=obraz, text="20 gr", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                        fontScale=0.5,
+                        color=(0, 255, 0), thickness=1)
+                suma=suma+0.20
+
+            #dla 50 gr
+            if i[2]==38 or i[2]==37:
+                cv2.putText(img=obraz, text="50 gr", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                        fontScale=0.5,
+                        color=(0, 255, 0), thickness=1)
+                suma=suma+0.50
+
+            #dla 1 zl
+            if i[2]==40 or i[2]==41 or i[2]==42:
+                cv2.putText(img=obraz, text="1 zl", org=(i[0], i[1]), fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                        fontScale=0.5,
+                        color=(0, 255, 0), thickness=1)
+                suma=suma+1.0
+
+    return obraz,suma
 "---------------------------------------------------------------------------------------------------------------------"
+#glowna petla
+for i in range(3):
+    obraz=zwrocObrazGithub(i)
+    drukujObraz("oryginalny",obraz)
 
-obraz=zwrocObrazGithub(0)
+    wszystkie=wszyskieNominaly(obraz.copy())
+    drukujObraz("wszystkie nomianly",wszystkie)
 
-wszystkie=wszyskieNominaly(obraz)
+    obraz_zloty,obraz_srebny=rodziel(wszystkie.copy())
+    drukujObraz("tylko zlote",obraz_zloty)
+    drukujObraz("tylko srebne",obraz_srebny)
 
-obraz_zloty,obraz_srebny=rodziel(wszystkie)
+    obraz_wykryty,suma=licz(obraz.copy(),obraz_zloty.copy(),obraz_srebny.copy())
+    drukujObraz("wykryte nominaly",obraz_wykryty)
+    print(suma)
 
 
-drukujObraz(obraz_zloty)
-
-drukujObraz(obraz_srebny)
 
 
 
